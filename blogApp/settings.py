055d712699c72c54fbf9c1b62eaff038520f341f
ctrl-root <muriithidennis340@gamil.com>
2023-05-15 14:24:11 +0300

    hosting on vercel

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-v+r480m@z6_x@hh4w#fq6t8zqrfo8+d@2823@eu7vntl!cz=%o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
